186ba2497f6f5d7621ffdb0d6070d7c8deef388a
Pedro <pedrosprandel27@gmail.com>
2022-08-23 02:42:49 -0300

    feat: header-v1

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Box, Divider, Flex, Link, Text } from '@chakra-ui/react';
+import { colors } from '../../utils';
 
 export const Header = () => {
     return (
-        <Text color="green">Texto</Text>
+        <Flex bg={colors.neutral} justifyContent="space-between" p="8">
+            <Text color={colors.light}>Header</Text>
+            <Flex color={colors.light} align="center" gap="4">
+                <Link href="https://google.com">Login</Link>
+                <Divider orientation="vertical" />
+                <Link border="1px solid" rounded="lg" px="2" py="1" href="https://google.com">
+                    <Text>Sign Up</Text>
+                </Link>
+            </Flex>
+        </Flex>
     )
 }
---
+++ client/src/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    neutral: "#050505",
+    primary: "#AC80A0",
+    secondary: "#E4572E",
+    light: "#F2F7F2",
+}
